27684fea55a781b6f1062ed57f6c86ca38bf3b66
Rob Blanckaert <basicer@basicer.com>
2018-07-13 17:14:31 -0700

    - Split out client - Fix channels - Improve reset code

---
+++ client.js
@@ -0,0 +1,261 @@
+const request = require("request-promise");
+const payloads = require("./payloads.js");
+const readline = require("readline");
+const WebSocket = require("ws");
+const chokidar = require("chokidar");
+const fs = require("fs");
+const qs = require("querystring");
+const Channel = require(`@nodeguy/channel`);
+
+let fo = o => o[Object.keys(o)[0]];
+
+class Client {
+  constructor(options) {
+    this.payload = payloads[options.language];
+    this.options = options;
+    this.read_channel = new Channel();
+  }
+  async auth() {
+    let resp = await request({
+      url: "https://repl.it/languages/" + this.payload.lang,
+      headers: {
+        //'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+        "User-Agent": "Mozilla/5.0 (repl.sh)"
+      },
+      simple: false,
+      jar: jar
+    });
+
+    let sessionJSON = resp.match(/__NEXT_DATA__ = ([^\n]+)/im);
+    let session = JSON.parse(sessionJSON[1]);
+    let repl = fo(session.props.initialState.repls.data);
+    this.token = repl.govalToken;
+    this.slug = repl.title;
+    this.replid = repl.id;
+  }
+
+  send(o) {
+    let client = this.client;
+    return new Promise(function(res, rej) {
+      client.send(JSON.stringify(o), function err(err) {
+        if (err) return rej(err);
+        else res();
+      });
+    });
+  }
+
+  read() {
+    return this.read_channel.shift();
+  }
+
+  async launch() {
+    let payshell = this.payload.shell.replace(
+      /\$\$TERM\$\$/g,
+      process.env.TERM || "xterm-color"
+    );
+
+    let files = [];
+    if (this.payload.main) {
+      files.push(
+        {
+          name: this.payload.main,
+          content: Buffer.from(payshell).toString("base64"),
+          encoding: "base64"
+        }
+      );
+    }
+
+    for (let f of this.options.file) {
+      let b = fs.readFileSync(f);
+      files.push({
+        //name: path.basename(f),
+        name: f,
+        content: b.toString("base64"),
+        encoding: "base64"
+      });
+      if (this.options.save) await this.commit(f, b);
+    }
+
+    if (this.payload.main) {
+      await this.send({ command: "runProject", data: JSON.stringify(files) });
+    } else {
+      await this.send({ command: "eval", data: payshell });
+    }
+    await new Promise((res) => setTimeout(res, 1000))
+    await this.send({
+      command: "resizeTerm",
+      data: JSON.stringify({cols: process.stdout.columns, rows: process.stdout.rows})
+    })
+    await this.send({
+      command: "input",
+      data: "echo -en '\\r' && stty sane\r" + (this.options.send ? this.options.send + "\r" : "") 
+    });
+  }
+
+  async commit(file, contents) {
+    let data;
+    try {
+      data = await request({
+        url: `https://repl.it/data/repls/signed_urls/${this.replid}/${qs.escape(
+          file
+        )}`,
+        jar: jar
+      });
+    } catch (e) {
+      spinner.fail("Couldnt get token to write " + file);
+      console.log(e);
+      return;
+    }
+    let target = JSON.parse(data).urls_by_action;
+    await request({
+      uri: target.write,
+      method: "PUT",
+      body: contents
+    });
+    spinner.info("Wrote " + file + " to GCS");
+  }
+
+  async connect() {
+    let client = new WebSocket("ws://" + this.options.goval + "/ws");
+    this.client = client;
+    this.clean = false;
+    this.buffer = [];
+    this.readers = [];
+
+    client.on("close", () => {
+      if (this.clean) return;
+      spinner.fail("Socket closed?");
+      return exit(1);
+    });
+
+    client.on("message", d => {
+      this.read_channel.push(JSON.parse(d));
+    });
+
+    await new Promise(function(res, rej) {
+      client.on("open", e => {
+        res(true);
+      });
+    });
+
+    let read = this.read.bind(this);
+    prompt("Sending Auth...");
+    await this.send({ command: "auth", data: this.token });
+    await read();
+    prompt("Waiting for ready...")
+    await this.send({ command: "select_language", data: this.payload.lang });
+    await read();
+  }
+
+  async go() {
+    //console.log(JSON.stringify(repl, null, '  '));
+    let read = this.read.bind(this);
+    let send = this.send.bind(this);
+    if (this.options.watch) {
+      this.watcher = chokidar.watch(this.options.file, {
+        awaitWriteFinish: true
+      });
+      this.watcher.on("change", (file) => {
+        if (this.options.reset) {
+          spinner.info(`File ${file} changed, restarting...`);
+          send({ command: "stop" }).then(() => this.launch());
+        } else {
+          let fj = {
+            name: file,
+            content: fs.readFileSync(file, "base64"),
+            encoding: "base64"
+          };
+
+          send({
+            command: "write",
+            data: JSON.stringify(fj)
+          });
+        }
+      });
+    }
+
+    this.attachTTY();
+    await this.run();
+  }
+
+  attachTTY() {
+    readline.emitKeypressEvents(process.stdin);
+    process.stdin.setRawMode(true);
+    let write_buffer = [];
+
+    process.stdin.on("keypress", (str, key) => {
+      //console.log(str, key);
+      if (key.sequence == "\u001d") {
+        exit(0);
+      }
+      write_buffer.push(key.sequence);
+    });
+
+    process.stdout.on("resize", () => {
+       this.send({
+        command: "resizeTerm",
+        data: JSON.stringify({cols: process.stdout.columns, rows: process.stdout.rows})
+      })
+    })
+
+    let writer = () => {
+      if (write_buffer.length > 0) {
+        this.send({ command: "input", data: write_buffer.join("") });
+        write_buffer = [];
+      }
+      if (!this.clean) setTimeout(writer, 5);
+    };
+    writer();
+  }
+
+  async run() {
+    let read = this.read.bind(this);
+    while (!this.clean) {
+      let d = await read();
+      if (d.command == "output") {
+        //console.log(d);
+        spinner.stop();
+        process.stdout.write(d.data);
+      } else if (d.command == "result") {
+        if (d.error) {
+          if ( d.error == 'unknown command "resizeTerm"' ) continue;
+          spinner.fail(d.error);
+        } else if (d.data) {
+          spinner.succeed(d.data);
+        }
+        if (process.stdin) {
+          process.stdin.setRawMode(false);
+          process.stdin.end();
+        }
+        this.disconnect()
+        return 0;
+      } else if (d.command == "ready") {
+        prompt("Got shell, waiting for prompt");
+      } else if (d.command == "event:portOpen") {
+        let j = JSON.parse(d.data);
+        spinner.succeed(
+          `Site open at https://${this.slug}--five-nine.repl.co (${j.port} -> 80)`
+        );
+      } else if (d.command == "event:packageInstallOutput") {
+        if (d.error) {
+          spinner.fail(d.error);
+        } else {
+          console.log(d.data);
+        }
+      } else {
+        if (d.error) {
+          spinner.fail(d.error);
+        } else if (["write", "files"].indexOf(d.command) == -1) {
+          spinner.info(d.command + ":" + d.data);
+        }
+      }
+    }
+  }
+  disconnect() {
+    this.clean = true;
+    this.client.close();
+    if (this.watcher) this.watcher.close();
+  }
+}
+
+module.exports = Client;
--- index.js
+++ index.js
@@ -2,15 +2,13 @@
 const path = require("path");
 const fs = require("fs");
 
-const WebSocket = require("ws");
+
 const glob = require("glob");
 const request = require("request-promise");
-const readline = require("readline");
 const ora = require("ora");
 const program = require("commander");
 const payloads = require("./payloads.js");
 const giparse = require("gitignore-globs");
-const chokidar = require("chokidar");
 const updateNotifier = require("update-notifier");
 const qs = require("querystring");
 
@@ -47,12 +45,12 @@ program
   )
   .parse(process.argv);
 
-let fo = o => o[Object.keys(o)[0]];
-let notifier = updateNotifier({ pkg: package });
-let jar = request.jar();
 
-let prompt = s => (spinner.text = s);
-const spinner = ora("Let's Go!").start();
+let notifier = updateNotifier({ pkg: package });
+global.jar = request.jar();
+
+global.prompt = s => (spinner.text = s);
+global.spinner = ora("Let's Go!").start();
 
 //Project mode
 if (program.P) {
@@ -98,7 +96,7 @@ if (program.P) {
   if (program.send) program.reset = true;
 }
 
-function exit(n) {
+global.exit = function exit(n) {
   notifier.notify();
   console.log("\nIf you like this, you might want to check out Repl.it");
   process.exit(n);
@@ -126,211 +124,18 @@ if (!payloads[program.language]) {
   exit(1);
 }
 
-let payload = payloads[program.language];
 
-async function launch(send) {
-  let payshell = payload.shell.replace(
-    /\$\$TERM\$\$/g,
-    process.env.TERM || "xterm-color"
-  );
-
-  if (payload.main) {
-    let files = [
-      {
-        name: payload.main,
-        content: Buffer.from(payshell).toString("base64"),
-        encoding: "base64"
-      }
-    ];
-
-    for (let f of program.file) {
-      let b = fs.readFileSync(f);
-      files.push({
-        //name: path.basename(f),
-        name: f,
-        content: b.toString("base64"),
-        encoding: "base64"
-      });
-      if (program.save) commit(f, b);
-    }
-
-    await new Promise((res, resj) => setTimeout(res, 10000));
-
-    await send({ command: "runProject", data: JSON.stringify(files) });
-  } else {
-    await send({ command: "eval", data: payshell });
-  }
-
-  await send({
-    command: "input",
-    data: "reset\r" + (program.send ? program.send : "") + "\r"
-  });
-}
-
-async function commit(file, contents) {
-  let data;
-  try {
-    data = await request({
-      url: `https://repl.it/data/repls/signed_urls/${replid}/${qs.escape(
-        file
-      )}`,
-      jar: jar
-    });
-  } catch (e) {
-    spinner.error("Couldnt get token to write " + file);
-    return;
-  }
-  let target = JSON.parse(data).urls_by_action;
-  await request({
-    uri: target.write,
-    method: "PUT",
-    body: contents
-  });
-  spinner.info("Wrote " + file + " to GCS");
-}
-
-(async function() {
-  prompt("Fetching free container from Repl.it ...");
-  let resp = await request({
-    url: "https://repl.it/languages/" + payload.lang,
-    headers: {
-      //'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
-      "User-Agent": "Mozilla/5.0 (repl.sh)"
-    },
-    simple: false,
-    jar: jar
-  });
-
-  let sessionJSON = resp.match(/__NEXT_DATA__ = ([^\n]+)/im);
-  let session = JSON.parse(sessionJSON[1]);
-  let repl = fo(session.props.initialState.repls.data);
-  let token = repl.govalToken;
-  let slug = repl.title;
-  replid = repl.id;
-
-  //console.log(JSON.stringify(repl, null, '  '));
-
-  let client = new WebSocket("ws://" + program.goval + "/ws");
-  let clean = false;
-  prompt("Connecting...");
-  client.on("close", function() {
-    if (clean) return;
-    spinner.fail("Socket closed?");
-    return exit(1);
-  });
-  await new Promise(function(res, rej) {
-    client.on("open", e => {
-      res(true);
-    });
-  });
-  let send = o =>
-    new Promise(function(res, rej) {
-      client.send(JSON.stringify(o), function err(err) {
-        if (err) return rej(err);
-        else res();
-      });
-    });
-  let read = () =>
-    new Promise(function(res, rej) {
-      client.once("message", d => {
-        res(JSON.parse(d));
-      });
-    });
-  prompt("Auth...");
-  await send({ command: "auth", data: token });
-  await read();
-  await send({ command: "select_language", data: payload.lang });
-  prompt("Starting shell.. " + (payload.main || "eval"));
-  await read();
-
-  await launch(send);
-  let w;
-  if (program.watch) {
-    w = chokidar.watch(program.file, {
-      awaitWriteFinish: true
-    });
-    w.on("change", function(file) {
-      console.log(file);
-      if (program.reset) {
-        spinner.info(`File ${file} changed, restarting...`);
-        send({ command: "stop" }).then(() => launch(send));
-      } else {
-        let fj = {
-          name: file,
-          content: fs.readFileSync(file, "base64"),
-          encoding: "base64"
-        };
-
-        send({
-          command: "write",
-          data: JSON.stringify(fj)
-        });
-      }
-    });
-  }
-
-  readline.emitKeypressEvents(process.stdin);
-  process.stdin.setRawMode(true);
-  let write_buffer = [];
-
-  process.stdin.on("keypress", (str, key) => {
-    //console.log(str, key);
-    if (key.sequence == "\u001d") {
-      exit(0);
-    }
-    write_buffer.push(key.sequence);
-  });
-
-  function writer() {
-    if (write_buffer.length > 0) {
-      send({ command: "input", data: write_buffer.join("") });
-      write_buffer = [];
-    }
-    if (!clean) setTimeout(writer, 5);
-  }
-  writer();
-
-  while (!clean) {
-    let d = await read();
-    if (d.command == "output") {
-      //console.log(d);
-      spinner.stop();
-      process.stdout.write(d.data);
-    } else if (d.command == "result") {
-      if (d.error) {
-        spinner.fail(d.error);
-      } else if (d.data) {
-        spinner.succeed(d.data);
-      }
-      if (process.stdin) {
-        process.stdin.setRawMode(false);
-        process.stdin.end();
-      }
-      clean = true;
-      client.close();
-      if (w) w.close();
-      return exit(0);
-    } else if (d.command == "ready") {
-      prompt("Got shell, waiting for prompt");
-    } else if (d.command == "event:portOpen") {
-      let j = JSON.parse(d.data);
-      spinner.succeed(
-        `Site open at https://${slug}--five-nine.repl.co (${j.port} -> 80)`
-      );
-    } else if (d.command == "event:packageInstallOutput") {
-      if (d.error) {
-        spinner.fail(d.error);
-      } else {
-        console.log(d.data);
-      }
-    } else {
-      if (d.error) {
-        spinner.fail(d.error);
-      } else if (["write", "files"].indexOf(d.command) == -1) {
-        spinner.info(d.command + ":" + d.data);
-      }
-    }
-  }
+;(async function() {
+    let Client = require('./client');
+    let client = new Client(program);
+    prompt("Fetching free container from Repl.it ...");
+    await client.auth();
+    prompt("Connecting...");
+    await client.connect();
+    prompt("Starting shell.. " + (client.payload.main || "eval"));
+    await client.launch();
+    await client.go();
+    exit(0);
 })().catch(function(err) {
   spinner.fail(err);
   if (err.stack) console.log(err.stack);
